a27ad7b4b0ff231e3a7a1967d0cba253a88b00e7
oleksandr-chornovol <Oleksandr.Chornovol@mheducation.com>
2024-12-16 11:57:31 +0200

    updated model

--- application.py
+++ application.py
@@ -6,8 +6,7 @@ from collections import Counter
 
 app = Flask(__name__)
 
-# Load YOLOv5 pre-trained model
-model = YOLO('yolov5x.pt')  # Use YOLOv5x pre-trained model
+model = YOLO('yolov5xu.pt') # Load YOLOv5 pre-trained model
 
 def detect_and_count(image):
     """
